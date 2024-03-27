077de644509924523d281413fdc60405df7fdf7d
francesca <francesca.giammichele@etiqa.it>
2024-03-27 10:24:28 +0100

    GEN-npm install and main fix for default theme error

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,38 @@
-import { Cart, Products } from './Products.tsx';
-import { Box, CssBaseline } from '@mui/material';
-import SearchAppBar from './SearchAppBar.tsx';
-import { Categories } from './Categories.tsx';
-import { useState } from 'react';
+import createTheme from "@mui/material/styles/createTheme";
+import { Box, CssBaseline } from "@mui/material";
+import SearchAppBar from "./SearchAppBar.tsx";
+import { Categories } from "./Categories.tsx";
+import { useState } from "react";
+import { Cart, Products } from "./Products.tsx";
+import { ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  // other theme properties
+});
 
 function App() {
-
   const [cart, setCart] = useState<Cart>();
-
 
   function onCartChange(cart: Cart) {
     setCart(cart);
   }
 
   return (
-    <Box height="100vh" display="flex" flexDirection="column">
-      <CssBaseline/>
-      <SearchAppBar quantity={cart?.totalItems || 0} price={cart?.totalPrice || 0}/>
-      <Box flex={1} display="flex" flexDirection="row">
-        <Categories/>
-        <Box flex={1}>
-          <Products onCartChange={onCartChange}/>
+    <ThemeProvider theme={theme}>
+      <Box height="100vh" display="flex" flexDirection="column">
+        <CssBaseline />
+        <SearchAppBar
+          quantity={cart?.totalItems || 0}
+          price={cart?.totalPrice || 0}
+        />
+        <Box flex={1} display="flex" flexDirection="row">
+          <Categories />
+          <Box flex={1}>
+            <Products onCartChange={onCartChange} />
+          </Box>
         </Box>
       </Box>
-
-    </Box>
+    </ThemeProvider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import './index.css';
-import './mocks/browser.ts';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { enableMockServiceWorker } from './mocks/browser.ts';
+import "./index.css";
+//import "./mocks/browser.ts";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { enableMockServiceWorker } from "./mocks/browser.ts";
+import App from "./App.tsx";
 
 enableMockServiceWorker().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
+  ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-      <App/>
-    </React.StrictMode>,
+      <App />
+    </React.StrictMode>
   );
 });
